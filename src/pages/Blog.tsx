b69c9d808f416c9149ec72e9b0f1ee787bae5629
Adarsh Mishra <40768847+iamadarsh1994@users.noreply.github.com>
2024-11-04 02:14:31 +0530

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,17 +1,17 @@
-// import React from 'react';
+import React from 'react';
 
-// const Blog: React.FC = (): JSX.Element => {
-//     return (
-// <>
+const Blog: React.FC = (): JSX.Element => {
+    return (
+<>
 
-// <div className='font-mono max-w-4xl mx-auto  overflow-auto min-h-screen bg-slate-400 text-slate-800 antialiased p-4'></div>
-// <div>Coming Soon...
-// </div>    
-// <hr />
+<div className='font-mono max-w-4xl mx-auto  overflow-auto min-h-screen bg-slate-400 text-slate-800 antialiased p-4'></div>
+<div>Coming Soon...
+</div>    
+<hr />
 
-// </>
+</>
         
-//     )
-// }
+    )
+}
 
-// export default Blog;
+export default Blog;
